f17b4a9c3eb94e9580df237ed98dacd3fad15529
raulserna <raul.sernamarin@telefonica.com>
2015-12-29 13:38:48 +0100

    phpmyadmin tool added

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,8 +2,7 @@
 
 # Environment variables
 PASSWORD_ROOT='1234'
-PASSWORD_GUEST='guest'
-PROJECTFOLDER='webproject'
+PROJECTFOLDER='src'
 
 # Update repo
 # sudo yum -y update
@@ -24,10 +23,10 @@ sudo yum install -y mysql mysql-server mysql-devel
 sudo mysqladmin -u root password ${PASSWORD_ROOT} 
 
 # Flush privileges
-sudo mysql -u root -p'1234' -e 'FLUSH PRIVILEGES';
+sudo mysql -u root -p${PASSWORD_ROOT} -e 'FLUSH PRIVILEGES';
 
 # Create new user guest on demo db
-sudo mysql -u root -p'1234' -e "USE mysql; create database demo; GRANT ALL PRIVILEGES ON demo.* TO 'guest'@'localhost' IDENTIFIED BY 'guest' WITH GRANT OPTION; UPDATE user SET Password=PASSWORD('guest') WHERE user='guest';" 
+sudo mysql -u root -p${PASSWORD_ROOT} -e "USE mysql; create database demo; GRANT ALL PRIVILEGES ON demo.* TO 'guest'@'localhost' IDENTIFIED BY 'guest' WITH GRANT OPTION; UPDATE user SET Password=PASSWORD('guest') WHERE user='guest';" 
 
 # Restart MySQL deamon
 /etc/init.d/mysqld restart
@@ -39,7 +38,7 @@ sudo yum install -y php php-mysql php-common php-gd php-mbstring php-mcrypt php-
 if [ ! -h /var/www ]; 
 then 
     rm -rf /var/www/html 
-    ln -s /vagrant/src /var/www/html
+    ln -s /vagrant/${PROJECTFOLDER} /var/www/html
     sed -i '/AllowOverride None/c AllowOverride All' /etc/httpd/conf/httpd.conf
     sudo /etc/init.d/httpd restart
 fi
@@ -49,6 +48,8 @@ sudo yum install -y phpmyadmin
 
 # Configure phpMyAdmin conf file
 sed -i '/Deny from All/c Allow from All' /etc/httpd/conf.d/phpMyAdmin.conf
+sed -i '/Allow from None/c Allow from All' /etc/httpd/conf.d/phpMyAdmin.conf
+sed -i '/Order Deny,Allow/c Order Allow,Deny' /etc/httpd/conf.d/phpMyAdmin.conf
 
 # Restart httpd
 sudo /etc/init.d/httpd restart
